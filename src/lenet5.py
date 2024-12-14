fb5a160df942368af6c5b5175cecc226caaa58ba
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2024-12-14 17:59:41 +0700

    Refactor lenet5.py

--- src/lenet5.py
+++ src/lenet5.py
@@ -1,28 +1,5 @@
-import torchvision
 import torch
 import torch.nn as nn
-import math
-
-
-# def get_resnet(model='resnet18'): 
-#     match model: 
-#         case 'resnet18':  
-#             model = torchvision.models.resnet18()
-#         case 'resnet34':
-#             model = torchvision.models.resnet34()
-#         case 'resnet50':
-#             model = torchvision.models.resnet50()
-#         case 'resnet101':
-#             model = torchvision.models.resnet101()
-    
-#     in_features = model.fc.in_features
-#     model.fc = torch.nn.Sequential(
-#         nn.Linear(in_features, 10),
-#     )
-
-#     model.conv1 = nn.Conv2d(1, 64, kernel_size=(7,7), stride=(2,2), padding=(3,3), bias=False)
-
-#     return model
 
 
 class LeNet5(nn.Module):
@@ -55,4 +32,3 @@ if __name__ == "__main__":
     input = torch.randn(1,1,28,28)
     print(model(input))
     
-    # print(model)
